ccff3abcb26e4ff0402e3c678b57e1206b39af6e
Olang Daniel <olangdan17@gmail.com>
2024-09-28 04:38:45 +0100

    install livewire and tallstackui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{--  <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -168,5 +168,16 @@
                 </div>
             </div>
         </div>
+    </body>
+</html>  --}}
+
+<html>
+    <head>
+        <tallstackui:script />
+        @livewireStyles
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        @livewireScripts
     </body>
 </html>
